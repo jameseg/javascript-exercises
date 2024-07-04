359278768b99d319a512b3494fef17dc3f7274c4
James Gooding <38703245+jameseg@users.noreply.github.com>
2024-07-04 18:04:36 -0400

    Complete palindromes exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = function (word) {
+
+  const alphanumeric = "abcdefghijklmnopqrstuvwxyz0123456789"
+  const cleanedString = word
+    .toLowerCase()
+    .split('')
+    .filter(char => alphanumeric.includes(char))
+    .join('')
+  const reversedString = cleanedString.split('').reverse().join('')
+
+  return cleanedString === reversedString
+
+
 
 };
+
+palindromes("racecar!")
 
 // Do not edit below this line
 module.exports = palindromes;
