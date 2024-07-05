4e70c27e2edee1d5e866afddadc08f9eb37f825b
James Gooding <38703245+jameseg@users.noreply.github.com>
2024-07-05 12:40:33 -0400

    Complete getoldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function (persons) {
 
-};
+  return persons.sort((a, b) => {
+    if (!a.yearOfDeath) {
+      a.yearOfDeath = new Date().getFullYear()
+    }
+
+    if (!b.yearOfDeath) {
+      b.yearOfDeath = new Date().getFullYear()
+    }
+    return ((a.yearOfDeath - a.yearOfBirth) < (b.yearOfDeath - b.yearOfBirth) ? 1 : -1)
+
+  })[0];
+}
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
